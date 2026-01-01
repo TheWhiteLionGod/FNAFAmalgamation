952a734135cb15cbf1db698105a5a0d1e3a1aa05
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-01-01 15:19:47 -0500

    Created Abstract Animatronic Class

---
+++ animatronics.gd
@@ -0,0 +1,27 @@
+"""
+Abstract Class to Handle Animatronic Behavior
+"""
+extends Skeleton3D
+class_name Animatronic
+
+enum Stage {
+	ZERO, ONE, TWO, KILL
+}
+
+var currentStage: Stage = Stage.ZERO
+
+# Making Animatronics Class Abstract
+func _init() -> void:
+	assert(get_script() != preload("res://animatronics.gd"), 
+	"Animatronic Class is abstract and cannot be instantiated.")
+
+# Making handleStage Method Abstract
+func handleStage() -> void:
+	@warning_ignore("assert_always_false")
+	assert(false, "Handle Stage Method is Abstract and Must be Overriten")
+
+func _ready() -> void:
+	currentStage = Stage.ZERO
+
+func _process(_delta: float) -> void:
+	handleStage()
