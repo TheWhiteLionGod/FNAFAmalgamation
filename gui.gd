f9e8edd1cf77d1f55299be6a904127e469794408
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-01-01 14:52:11 -0500

    Improved Game State and Camera System
    Created Enum for Cameras
    Moved Activate Camera Variable into Game State
    Modified Event Node into Global Game State Node
    Created Switch Camera Signal
    Code more readable

--- gui.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-@export var Events: Node3D
-
-func _ready() -> void:
-	if Events == null:
-		print("ERROR: Export Variable 'Events' Not Defined")
--- timer.gd
+++ timer.gd
@@ -1,14 +1,25 @@
+"""
+Modifies the Timer Label with the Time from Game State
+"""
 extends MarginContainer
 
-@onready var GUI: CanvasLayer = $".."
 @onready var label: Label = $"./Time"
-@onready var Events: Node3D = GUI.Events
+var timer: float = 0.0
 
-func _process(_delta: float) -> void:
-	var minutes: int = floor(Events.globalTimer / 60)
-	var seconds: int = int(Events.globalTimer) % 60
-	var milliseconds: int = int(round((Events.globalTimer - int(Events.globalTimer))*10)) % 10
+func updateText() -> void:
+	var minutes: int = floor(timer / 60)
+	var seconds: int = int(timer) % 60
+	var milliseconds: int = int(round((timer - int(timer))*10)) % 10
+	
 	label.text = str(minutes) + ":" + \
 				("0" if seconds < 10 else "") + \
 				str(seconds) + \
 				"." + str(milliseconds)
+
+func _ready() -> void:
+	timer = 0
+
+func _process(delta: float) -> void:
+	timer += delta
+	GameState.globalTimer = timer
+	updateText()
