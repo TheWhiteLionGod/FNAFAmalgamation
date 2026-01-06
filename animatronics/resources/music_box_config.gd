b14d568f4a8185b1589534d71beafdf211b3a79e
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-01-06 10:07:10 -0500

    Removed Properties Scene for Custom Resource

---
+++ animatronics/resources/music_box_config.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name MusicBoxConfig
+
+@export var windUpUnitsPerSecond: int = 500;
+@export var windDownUnitsPerSecond: int = 80;
+@export var totalUnits: int = 2000;
--- animatronics/scripts/music_box.gd
+++ animatronics/scripts/music_box.gd
@@ -2,28 +2,23 @@ extends StaticBody3D
 
 @export var main: Node3D
 @export var textureProgressBar: TextureProgressBar
+@export var config: MusicBoxConfig
 
 @onready var camera: Camera3D = main.get_node("Camera")
 
 @onready var modules: Node = main.get_node("Modules")
-@onready var properties: Dictionary = modules.get_node("Properties").properties
 @onready var Raycaster: Node3D = modules.get_node("Raycaster")
-
-@onready var musicBoxProperties: Dictionary = properties["music_box"]
-@onready var windDownUnitsPerSecond: int = musicBoxProperties["Wind_Down_Units_Per_Second"]
-@onready var totalUnits: int = musicBoxProperties["Total_Units"]
-@onready var windUpUnitsPerSecond: int = musicBoxProperties["Wind_Up_Units_Per_Second"]
 
 @onready var model: Node3D = get_node("Model")
 @onready var winder: Node3D = model.get_node("Winder")
 
-@onready var currentUnits: float = totalUnits
+@onready var currentUnits: float = config.totalUnits
 var isWindingUp: bool = false
 
 func _ready() -> void:
 	textureProgressBar.min_value = 0
-	textureProgressBar.max_value = totalUnits
-	textureProgressBar.step = totalUnits / 10.0
+	textureProgressBar.max_value = config.totalUnits
+	textureProgressBar.step = config.totalUnits / 10.0
 	textureProgressBar.value = currentUnits
 
 func _process(delta: float) -> void:
@@ -41,13 +36,13 @@ func _process(delta: float) -> void:
 	
 	# Wind down
 	if not isWindingUp:
-		currentUnits -= windDownUnitsPerSecond * delta
-		currentUnits = clamp(currentUnits, 0, totalUnits)
+		currentUnits -= config.windDownUnitsPerSecond * delta
+		currentUnits = clamp(currentUnits, 0, config.totalUnits)
 
 		winder.rotation_degrees.x -= 1
 	else:
-		currentUnits += windUpUnitsPerSecond * delta
-		currentUnits = clamp(currentUnits, 0, totalUnits)
+		currentUnits += config.windUpUnitsPerSecond * delta
+		currentUnits = clamp(currentUnits, 0, config.totalUnits)
 
 		winder.rotation_degrees.x += 1
 	
