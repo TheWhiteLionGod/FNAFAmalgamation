7e7d89be4075474194829170cd73825ee7ee7de0
SavageMode123 <3034999@edison.k12.nj.us>
2026-01-15 09:20:36 -0500

    Made door animation

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+@export var main: Node3D
+
+@onready var rooms: Node3D = main.get_node("Rooms")
+
 var adjustingMask: bool = false
 
 @onready var equipMaskAnimation: Animation = $"AnimationPlayer".get_animation("equip_mask")
@@ -15,9 +19,19 @@ func _input(event: InputEvent) -> void:
 
 	elif event.is_action_pressed("left_door"):
 		GameState.playerActions["left_door"] = !GameState.playerActions["left_door"]
+
+		if GameState.playerActions["left_door"]:
+			rooms.get_node("AnimationPlayer").play("close_left_door")
+		else:
+			rooms.get_node("AnimationPlayer").play("open_left_door")
 	
 	elif event.is_action_pressed("right_door"):
 		GameState.playerActions["right_door"] = !GameState.playerActions["right_door"]
+
+		if GameState.playerActions["right_door"]:
+			rooms.get_node("AnimationPlayer").play("close_right_door")
+		else:
+			rooms.get_node("AnimationPlayer").play("open_right_door")
 	
 	elif event.is_action_pressed("cameras"):
 		GameState.playerActions["cameras"] = !GameState.playerActions["cameras"]
