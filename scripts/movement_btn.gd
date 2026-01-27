4be1eca16c8baf2bd5babda0979c016950390488
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-01-27 12:10:34 -0500

    Created Movement Buttons

---
+++ scripts/movement_btn.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+func _turn_left() -> void:
+	print("Turn Left")
+	pass # Replace with function body.
+
+func _turn_right() -> void:
+	print("Turn Right")
+	pass # Replace with function body.
+
+func _look_up() -> void:
+	print("Look Up")
+	pass # Replace with function body.
+
+func _mask_btn() -> void:
+	print("Mask")
+	pass # Replace with function body.
+
+func _cam_btn() -> void:
+	print("Camera")
+	pass # Replace with function body.
