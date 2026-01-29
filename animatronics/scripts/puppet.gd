24e9506ed24c5a3e60b4f61d98dc5b9895feaff4
SavageMode123 <3034999@edison.k12.nj.us>
2026-01-29 11:23:43 -0500

    Fixed puppet jumpscare

--- animatronics/scripts/puppet.gd
+++ animatronics/scripts/puppet.gd
@@ -33,7 +33,7 @@ func handleStage() -> void:
 			GameState.playerActions["direction"] = GameState.Facing.MUSIC_BOX
 			GameState.turn.emit()
 
-			jumpscare(0.8, 1.7, Vector3(0.05, -1.55, -2.4))
+			jumpscare(0.8, 1.7, Vector3(2.4,-1.55,0.075))
 			playerKilled()
 
 		_:
