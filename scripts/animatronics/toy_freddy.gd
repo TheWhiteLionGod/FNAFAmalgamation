2bcfca8f48623634f4378659ba3b9d318b0d6138
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-02-03 13:30:32 -0500

    Created Toy Freddy Config Resource

--- scripts/animatronics/toy_freddy.gd
+++ scripts/animatronics/toy_freddy.gd
@@ -3,26 +3,34 @@ This Script Will Control Toy Freddy Behavior
 """
 extends Animatronic
 
+signal configReady
 @export_range(0, 20) var AI_LEVEL: int = 20
+@export var config: ToyFreddyConfig:
+	set(value):
+		config = value
+		configReady.emit()
+
 var hasMoved: bool = false
 
 @onready var killStage: int = getKillStage()
 
 func _init():
-	super(3)
+	await configReady
+	super(config.stages)
 
 func _ready() -> void:
 	super._ready()
 	GameState.blackoutEnd.connect(checkForKill)
 
 func handleStage() -> void:
+	print(killStage)
 	match currentStage:
 		0, 1:
 			var curTime = GameState.globalTimer
 			curTime = round(curTime * 10) / 10
 			
 			# NOT Movement Opportunity (10.0, 20.0, 30.0, etc)
-			if int(curTime) % 2 != 0 || curTime - int(curTime) != 0:
+			if int(curTime) % config.movementInterval != 0 || curTime - int(curTime) != 0:
 				hasMoved = false # Resetting Boolean
 				return
 
@@ -35,13 +43,16 @@ func handleStage() -> void:
 				return; # Failed Movement
 
 			# Progressing Stage
+			if GameState.playerActions["in_blackout"] && currentStage + 1 == killStage:
+				return; # Failing Movement If Blackout is Occuring
+
 			currentStage += 1
 			moveToStageMarker()
 
 		killStage:
 			if !GameState.playerActions["in_blackout"]:
 				GameState.playerActions["in_blackout"] = true
-				GameState.blackoutStart.emit(5)
+				GameState.blackoutStart.emit(config.blackoutLength)
 
 		_:
 			print(
@@ -55,5 +66,5 @@ func checkForKill() -> void:
 		moveToStageMarker()
 		return
 
-	jumpscare(0.8, 1.7)
+	jumpscare(config.intensity, config.decayRate, config.jumpscarePosOffset)
 	playerKilled()
