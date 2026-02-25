1ab6b0b11d74c468fa500638e892a30c3860b6e0
SavageMode123 <3034999@edison.k12.nj.us>
2026-02-25 10:48:30 -0500

    Remove test.gd

--- test.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@onready var db: Database = Database.new(get_tree().current_scene)
-
-func _ready() -> void:
-	# Small delay to ensure the node is fully in the scene tree
-	await get_tree().process_frame
-	db.addData(["Test", 500])
