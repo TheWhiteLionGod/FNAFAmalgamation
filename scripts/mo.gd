5d7622f0650f2dae3bea5e7ff5b0d52b0b67f8c3
SavageMode123 <3034999@edison.k12.nj.us>
2026-03-04 09:24:52 -0500

    Fixed Movement Opportunity false check pass when currTime is 0 (most typically found when game is restarted) Updated Toy Chica Animations

--- scripts/mo.gd
+++ scripts/mo.gd
@@ -5,23 +5,24 @@ var isDone: bool
 
 @warning_ignore("SHADOWED_VARIABLE")
 func _init(interval: int):
-    self.interval = interval
-    isDone = false
+	self.interval = interval
+	isDone = false
 
 func check(AI_LEVEL: int) -> bool:
-    var curTime = GameState.globalTimer
-    curTime = round(curTime * 10) / 10
-    
-    # NOT Movement Opportunity
-    if int(curTime) % interval != 0 or curTime - int(curTime) != 0:
-        isDone = false # Resetting Boolean
-        return false
+	var curTime = GameState.globalTimer
+	curTime = round(curTime * 10) / 10
+	
+	# NOT Movement Opportunity
+	if (int(curTime) % interval != 0 or curTime - int(curTime) != 0) and curTime != 0:
+		isDone = false # Resetting Boolean
+		return false
 
-    if isDone:
-        return false
-    isDone = true
+	if isDone:
+		return false
 
-    if randi_range(0, 20) >= AI_LEVEL:
-        return false # Failed Movement
+	isDone = true
 
-    return true
+	if randi_range(0, 20) >= AI_LEVEL:
+		return false # Failed Movement
+
+	return true
