f75c41b22b864bf238800dbc29e6794e20043e94
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-03-05 13:13:44 -0500

    Made Ballora Animatronic

---
+++ resources/ballora_config.gd
@@ -0,0 +1,13 @@
+"""
+Config For Toy Freddy
+"""
+extends Resource
+class_name BalloraConfig
+
+@export var stages: int = 12;
+@export var movementInterval: int = 10;
+
+@export_group("Jumpscare")
+@export var intensity: float = 0.1
+@export var decayRate: float = 1.7
+@export var direction: GameState.Facing = GameState.Facing.OFFICE
