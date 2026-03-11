85da27b313b3fd5a06cdef2d2cebaf7dde787e83
SavageMode123 <3034999@edison.k12.nj.us>
2026-03-11 13:04:48 -0400

    Updated Balloon Boy Jumpscare Marker

--- scripts/player/camera.gd
+++ scripts/player/camera.gd
@@ -7,6 +7,7 @@ const CAMERA_ROTATION_SPEED_PER_SECOND: float = 5 # In Degrees
 const CAMERA_MAX_ROTATION: float = 20 # In Degrees
 
 var rotateSign: int = 1
+var cameraOpenedTime: float = 0
 
 @onready var cameras: Array[Node] = get_children()
 @onready var camInitalRotation: Array[Vector3] = []
@@ -50,6 +51,10 @@ func changeCamera(camera: GameState.Camera) -> void:
 	guiAnimationPlayer.play("camera_open")
 
 func openCameras() -> void:
+	if GameState.globalTimer - cameraOpenedTime < 0.5:
+		return
+
+	cameraOpenedTime = GameState.globalTimer
 	cameraGui.show()
 	cameraShaders.show()
 	GameState.curCamNode.current = true
