993bd1f500ab78da9e4771bc3508756bffa392a0
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-06-30 08:29:35 -0400

    Created Generic Room Scene

---
+++ scripts/room.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var camera: Camera3D = $"Camera3D"
+# @onready var mesh: Node3D = $"Mesh"
+var markers: Array[Marker3D]
+var occupants: Dictionary[Marker3D, String]
+
+func _ready() -> void:
+	var marker_folder: Node3D = $"Markers"
+	for child in marker_folder.get_children():
+		if child is not Marker3D:
+			continue	
+		
+		var marker: Marker3D = child as Marker3D
+		markers.append(marker)
+		occupants[marker] = ""
+
+	print(occupants)
